528a694999d7dda148abe5c871c614cce9120827
erentrk01 <erentarak1907@gmail.com>
2022-09-09 12:13:45 +0300

    tagline fix

--- components/HouseList/houses.js
+++ components/HouseList/houses.js
@@ -2,26 +2,26 @@ export  const houses = [
 	{
 		name:"Family Home",
 		tagline:"Starting at $69,420",
-		taglineCTA:"Touchless Delivery",
+		taglineCTA:"Oakland, CA",
 		image:require("../../assets/images/SolarRoof.jpeg"),
 	},
 	{
 		name:"Model 3",
-		tagline:"Order Online for",
-		taglineCTA:"Touchless Delivery",
+		tagline:"Starting at $89,420",
+		taglineCTA:"Boston, MA",
 		image:require("../../assets/images/home2.jpeg"),
 
 	},
 	{
 		name:"Model X",
-		tagline:"Order Online for",
-		taglineCTA:"Touchless Delivery",
+		tagline:"Starting at $120,220",
+		taglineCTA:"Atlanta, GA",
 		image:require("../../assets/images/home3.jpg"),
 	},
 	{
 		name:"Model Y",
-		tagline:"Order Online for",
-		taglineCTA:"Touchless Delivery",
+		tagline:"Starting at $89,420",
+		taglineCTA:"Austin, TX",
 		image:require("../../assets/images/home4.jpeg"),
 	}
 ];
