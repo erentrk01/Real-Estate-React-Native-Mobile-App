f1c240dddfa234805ea2f3cf2e9bc676817fefe0
erentrk01 <erentarak1907@gmail.com>
2022-09-09 11:21:49 +0300

    navigation header

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
 import HouseList from './components/HouseList/index';
 
 export default function App() {
   return (
+
 	<View style={styles.container}>
+		<Header></Header>
 		<HouseList></HouseList>
 		<StatusBar style="auto" />
 	</View>
