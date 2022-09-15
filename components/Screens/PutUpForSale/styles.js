6aa3239ee657d9d198b5374ba7c7fc450067192b
erentrk01 <erentarak1907@gmail.com>
2022-09-15 11:43:08 +0300

    putupfor sale

---
+++ components/Screens/PutUpForSale/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+	header:{
+		fontWeight:'bold',
+		marginLeft:20,
+		marginTop:103
+	},
+	textInput:{
+		height:40,
+		borderWidth:1,
+		paddingLeft:20,
+		margin:5,
+		borderColor:'black'
+	},
+	seperator:{
+		marginVertical: 5,
+		borderBottomColor: '#FFCE33',
+		borderBottomWidth: 2
+	}
+})
+
+export default  styles;
