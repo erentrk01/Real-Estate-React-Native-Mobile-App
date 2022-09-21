97a31336412bb279e85019b57fc45121a60839ea
erentrk01 <erentarak1907@gmail.com>
2022-09-21 22:48:37 +0300

    redux settings

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-//import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +20,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+
+export const authentication = getAuth(app);
 
 export default getFirestore();
