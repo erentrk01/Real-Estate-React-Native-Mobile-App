2ddd91640d3d32ea723d997db771d4b4a5583799
erentrk01 <erentarak1907@gmail.com>
2022-09-28 22:04:05 +0300

    profile screen nav

--- components/Navigation/AuthStack.js
+++ components/Navigation/AuthStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text} from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+i
 import AnimatedAuth from "../Animated-Auth/index";
 import AppStack from "./AppStack";
 import CategoryStack from "./CategoryStack";
